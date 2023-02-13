affbc8c8de133f5b92aa3930f957e7692dcc19ec
RICHHAPAL CHOUDHARY <richhapal.16jccs098@jietjodhpur.ac.in>
2023-02-13 22:53:16 +0530

    Make responsive

--- src/components/Weather/WeeklyContent.jsx
+++ src/components/Weather/WeeklyContent.jsx
@@ -5,7 +5,17 @@ import "../../sass/weeklyweather.scss";
 const WeeklyContent = () => {
      return (
           <Box className="weeklyWeatherDivision">
-               <h1>HelloASDFASDFASDF</h1>
+               <Box className="weeklyWeatherDivision-heading">
+                    <h4>7-DAY FORECAST</h4>
+               </Box>
+
+               <Box className="weeklyWeatherDivision-data">
+                    <Box className="weeklyWeatherDivision-row">
+                         <Box className="weeklyWeatherDivision-row_day">Monday</Box>
+                         <Box className="weeklyWeatherDivision-row_weather">ICON</Box>
+                         <Box className="weeklyWeatherDivision-row_temp">25/21</Box>
+                    </Box>
+               </Box>
           </Box>
      );
 };
